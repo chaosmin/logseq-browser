2cd0d83c500fe484216599b37f5e4f45b61ef72b
Hugo Min <chaosmin@hotmail.com>
2026-04-27 10:22:35 +0800

    feat: chokidar file watcher for hot index updates

--- watcher.js
+++ watcher.js
@@ -1,3 +1,28 @@
 'use strict';
-function startWatcher() {}
+
+const chokidar = require('chokidar');
+const path = require('path');
+const { updateFile, removeFile } = require('./indexer');
+
+function startWatcher(rootDir) {
+  const watcher = chokidar.watch(
+    [
+      path.join(rootDir, 'pages', '**', '*.md'),
+      path.join(rootDir, 'journals', '**', '*.md'),
+    ],
+    {
+      ignoreInitial: true,
+      followSymlinks: false,
+      ignored: /(^|[/\\])\../,
+    }
+  );
+
+  watcher
+    .on('add', filePath => { console.log(`[watcher] added: ${filePath}`); updateFile(filePath); })
+    .on('change', filePath => { console.log(`[watcher] changed: ${filePath}`); updateFile(filePath); })
+    .on('unlink', filePath => { console.log(`[watcher] removed: ${filePath}`); removeFile(filePath); });
+
+  return watcher;
+}
+
 module.exports = { startWatcher };
